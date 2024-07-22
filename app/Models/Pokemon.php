ca1b482db51d8bb9123ea882bbb73bfc34037de3
tpalinski <tpalinski@icloud.com>
2024-07-22 09:24:28 +0200

    Add favourite pokemon to model

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Pokemon extends Model
 {
@@ -35,4 +36,8 @@ class Pokemon extends Model
         'special_def' => 0,
         'image' => ""
     ];
+
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -18,7 +19,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'password',
     ];
 
@@ -40,8 +40,12 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
+
+    public function favourites(): BelongsToMany {
+        return $this->belongsToMany(Pokemon::class);
+    }
+
 }
