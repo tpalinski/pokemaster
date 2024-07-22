42da328448511ef053e80045a2afb14223c90699
tpalinski <tpalinski@icloud.com>
2024-07-22 12:53:44 +0200

    Add explicit icon import

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-          @vite('resources/css/app.css')
+        @vite('resources/css/app.css')
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
     </head>
     <body class="font-sans antialiased bg-yellow-400 flex flex-col items-center">
         <img src="{{asset("img/logo.png")}}" alt="Pokemaster logo" />
